2695c92527ebec7e2cb069650d2daf5e2b6a6a07
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2025-09-16 12:16:34 -0500

    display list with toString

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -3,15 +3,38 @@ package com.example.listviewexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import model.Pet;
 
 public class MainActivity extends AppCompatActivity {
+    ListView petListWidget;
+    ArrayList<Pet> pets = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int selection = 1;
+        pets = new ArrayList<>();
+        pets.add(new Pet("German Shepherd", "Rex", 5));
+        pets.add(new Pet("Bulldog", "Bruno", 3));
+        pets.add(new Pet("Beagle", "Bella", 4));
+        pets.add(new Pet("Poodle", "Luna", 2));
+        pets.add(new Pet("Golden Retriever", "Max", 6));
+        pets.add(new Pet("Dachshund", "Charlie", 1));
+        pets.add(new Pet("Boxer", "Rocky", 4));
+        pets.add(new Pet("Rottweiler", "Duke", 5));
+        pets.add(new Pet("Siberian Husky", "Molly", 3));
+        pets.add(new Pet("Doberman", "Zoe", 2));
+
+        petListWidget = findViewById(R.id.listViewWidg);
+        Random rand = new Random();
+        int selection = 2;
         populateListView(selection);
     }
 
@@ -32,14 +55,16 @@ public class MainActivity extends AppCompatActivity {
      * the content of a strings array resource
      */
     private void useStringResource() {
-
+        String[] contents = getResources().getStringArray(R.array.breeds);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, contents);
+        petListWidget.setAdapter(adapter);
     }
 
     /**
      * This method populates the list view using a string resource containing the list of a data array
      */
     private void usingDataArray() {
-
-
+        ArrayAdapter<Pet> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, pets);
+        petListWidget.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/model/Pet.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Pet {
+    private String breed;
+    private String name;
+    private int age;
+
+    public Pet() {
+    }
+    public Pet(String breed, String name, int age) {
+        this.breed = breed;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + breed + "), Age: " + age;
+    }
+}
